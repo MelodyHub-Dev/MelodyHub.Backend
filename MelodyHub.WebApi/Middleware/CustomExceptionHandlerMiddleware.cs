202f657a0262c8dc9599ef85bd7ad016b1e507c3
artxmix <artem.artusevskij01@gmail.com>
2026-02-01 21:27:05 +0300

    feat: Add NotFoundException and middleware for process exceptions

---
+++ MelodyHub.WebApi/Middleware/CustomExceptionHandlerMiddleware.cs
@@ -0,0 +1,41 @@
+﻿using MelodyHub.Application.Common.Exceptions;
+using System.Net;
+using System.Text.Json;
+
+namespace MelodyHub.WebApi.Middleware;
+
+public class CustomExceptionHandlerMiddleware(RequestDelegate next)
+{
+    public async Task Invoke(HttpContext context)
+    {
+        try
+        {
+            await next(context);
+        }
+        catch (Exception exception)
+        {
+            await HandleExceptionAsync(context, exception);
+        }
+    }
+
+    private static Task HandleExceptionAsync(HttpContext context, Exception exception)
+    {
+        var code = HttpStatusCode.InternalServerError;
+        var result = string.Empty;
+        switch (exception)
+        {
+            case NotFoundException:
+                code = HttpStatusCode.NotFound;
+                break;
+        }
+        context.Response.ContentType = "application/json";
+        context.Response.StatusCode = (int)code;
+
+        if (result == string.Empty)
+        {
+            result = JsonSerializer.Serialize(new { errpr = exception.Message });
+        }
+
+        return context.Response.WriteAsync(result);
+    }
+}
