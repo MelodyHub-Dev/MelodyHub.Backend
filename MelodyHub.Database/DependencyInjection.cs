7d7003b3d7e51fe5e2f6d1be86b8a506f329c1fe
artxmix <artem.artusevskij01@gmail.com>
2026-02-01 22:09:18 +0300

    feat: Add start app config

---
+++ MelodyHub.Database/DependencyInjection.cs
@@ -0,0 +1,32 @@
+﻿using MelodyHub.Application.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MelodyHub.Database;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddDatabase(this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        var connectionString = configuration["DbConnection"];
+        var serverVersion = new MySqlServerVersion(new Version(configuration["DatabaseSettings:ServerVersion"]
+            ?? throw new NullReferenceException("server version was null")));
+
+        services.AddDbContext<MelodyHubDbContext>(options =>
+        {
+            options.UseMySql(
+                connectionString,
+                serverVersion,
+                mySqlOptions => mySqlOptions.EnableStringComparisonTranslations()
+            );
+        });
+
+        services.AddScoped<IMelodyHubDbContext>(provider =>
+            provider.GetService<MelodyHubDbContext>()
+                ?? throw new NullReferenceException("provider cant't be null"));
+
+        return services;
+    }
+}
--- MelodyHub.WebApi/Program.cs
+++ MelodyHub.WebApi/Program.cs
@@ -1,8 +1,45 @@
+using MelodyHub.Application;
+using MelodyHub.Application.Common.Mappings;
+using MelodyHub.Application.Interfaces;
+using MelodyHub.Database;
 using MelodyHub.WebApi.Middleware;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddOpenApi();
+builder.Services.AddControllers();
+builder.Services.AddAutoMapper(config =>
+{
+    config.AddProfile(new AssemblyMappingProfile(Assembly.GetExecutingAssembly()));
+    config.AddProfile(new AssemblyMappingProfile(typeof(IMelodyHubDbContext).Assembly));
+});
+builder.Services.AddApplication();
+builder.Services.AddDatabase(builder.Configuration);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("FrontendPolicy", policy =>
+    {
+        policy.WithOrigins("http://localhost:3000")
+              .AllowAnyHeader()
+              .AllowAnyMethod()
+              .AllowCredentials();
+    });
+});
+
 var app = builder.Build();
 
+using var scope = app.Services.CreateScope();
+var context = scope.ServiceProvider.GetRequiredService<MelodyHubDbContext>();
+Console.WriteLine(context.Materials);
+
+DbContextInitializer.Initialize(context);
+
 app.UseCustomExceptionHandler();
+app.UseRouting();
+app.UseHttpsRedirection();
+app.UseCors("FrontendPolicy");
+app.MapControllers();
 
 app.Run();
