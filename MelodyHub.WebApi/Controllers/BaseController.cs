e4022ad96ea953b65eaee197e639e05cf08ea09c
artxmix <artem.artusevskij01@gmail.com>
2026-02-01 21:30:05 +0300

    feat: Add BaseController

---
+++ MelodyHub.WebApi/Controllers/BaseController.cs
@@ -0,0 +1,35 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace MelodyHub.WebApi.Controllers;
+
+[ApiController]
+[Route("api/[controller]/[action]")]
+public abstract class BaseController : ControllerBase
+{
+    private IMediator _mediator = null!;
+
+    protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>()
+        ?? throw new NullReferenceException("Mediator is null.");
+
+    protected Guid UserId
+    {
+        get
+        {
+            if (!User.Identity?.IsAuthenticated ?? false)
+                return Guid.Empty;
+
+            var userIdClaim = User.FindFirst(ClaimTypes.NameIdentifier)
+                ?? User.FindFirst("sub")
+                ?? User.FindFirst("uid");
+
+            if (userIdClaim == null || string.IsNullOrWhiteSpace(userIdClaim.Value))
+                return Guid.Empty;
+
+            return Guid.TryParse(userIdClaim.Value, out var userId)
+                ? userId
+                : Guid.Empty;
+        }
+    }
+}
--- MelodyHub.WebApi/Program.cs
+++ MelodyHub.WebApi/Program.cs
@@ -3,7 +3,6 @@ using MelodyHub.WebApi.Middleware;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-
 app.UseCustomExceptionHandler();
 
 app.Run();
