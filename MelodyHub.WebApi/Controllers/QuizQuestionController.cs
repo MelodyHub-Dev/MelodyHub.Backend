36e2ae177feb82c12cd734d6945541367befba30
artxmix <artem.artusevskij01@gmail.com>
2026-02-05 23:58:32 +0300

    feat: Add QuizQuestionController

---
+++ MelodyHub.WebApi/Controllers/QuizQuestionController.cs
@@ -0,0 +1,59 @@
+﻿using AutoMapper;
+using MelodyHub.Application.CQRS.QuizQuestions.Commands.CreateQuizQuestion;
+using MelodyHub.Application.CQRS.QuizQuestions.Commands.DeleteQuizQuestion;
+using MelodyHub.Application.CQRS.QuizQuestions.Commands.UpdateQuizQuestion;
+using MelodyHub.Application.CQRS.QuizQuestions.Queries.GetQuizQuestionList;
+using MelodyHub.WebApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MelodyHub.WebApi.Controllers;
+
+[Route("/api/quiz-questions")]
+public class QuizQuestionController(IMapper mapper) : BaseController
+{
+    [HttpGet("{id:guid}")]
+    public async Task<ActionResult<QuizQuestionListVm>> Get(Guid id)
+    {
+        var query = new GetQuizQuestionListQuery
+        {
+            QuizId = id
+        };
+
+        var quizQuiestions = await Mediator.Send(query);
+
+        return Ok(quizQuiestions);
+    }
+
+    [HttpPost("create")]
+    public async Task<ActionResult<Guid>> Create([FromBody] CreateQuizQuestionDto dto)
+    {
+        var command = mapper.Map<CreateQuizQuestionCommand>(dto);
+
+        var quizQuestionId = await Mediator.Send(command);
+
+        return Ok(quizQuestionId);
+    }
+
+    [HttpPut("update")]
+    public async Task<ActionResult> Update([FromBody] UpdateQuizQuestionDto dto)
+    {
+        var command = mapper.Map<UpdateQuizQuestionCommand>(dto);
+
+        await Mediator.Send(command);
+
+        return NoContent();
+    }
+
+    [HttpDelete("delete/{id:guid}")]
+    public async Task<ActionResult> Delete(Guid id)
+    {
+        var command = new DeleteQuizQuestionCommand
+        {
+            QuizQuestionId = id
+        };
+
+        await Mediator.Send(command);
+
+        return NoContent();
+    }
+}
---
+++ MelodyHub.WebApi/Models/UpdateQuizQuestionDto.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using MelodyHub.Application.Common.Mappings;
+using MelodyHub.Application.CQRS.QuizQuestions.Commands.UpdateQuizQuestion;
+
+namespace MelodyHub.WebApi.Models;
+
+public class UpdateQuizQuestionDto : IMapWith<UpdateQuizQuestionCommand>
+{
+    public Guid Id { get; set; }
+    public Guid QuizId { get; set; }
+    public string QuestionText { get; set; } = string.Empty;
+    public string OptionA { get; set; } = string.Empty;
+    public string OptionB { get; set; } = string.Empty;
+    public string? OptionC { get; set; }
+    public string? OptionD { get; set; }
+    public char CorrectAnswer { get; set; } // 'a', 'b', 'c', 'd'
+    public string? Explanation { get; set; }
+    public byte Points { get; set; } = 10;
+
+    public void Mapping(Profile profile)
+        => profile.CreateMap<UpdateQuizQuestionDto, UpdateQuizQuestionCommand>();
+}
