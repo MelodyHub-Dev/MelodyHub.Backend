1dacbe17f676812291760940406e2df7b8e493bc
artxmix <artem.artusevskij01@gmail.com>
2026-02-08 17:41:41 +0300

    feat: Add QuizResultController

---
+++ MelodyHub.WebApi/Controllers/QuizResultController.cs
@@ -0,0 +1,48 @@
+﻿using AutoMapper;
+using MelodyHub.Application.CQRS.QiuzResults.Commands.CreateQuizResult;
+using MelodyHub.Application.CQRS.QiuzResults.Commands.DeleteQuizResult;
+using MelodyHub.Application.CQRS.QiuzResults.Queries.GetQuizResultList;
+using MelodyHub.WebApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MelodyHub.WebApi.Controllers;
+
+[Route("/api/quiz-results")]
+public class QuizResultController(IMapper mapper) : BaseController
+{
+    [HttpGet("{id:guid}")]
+    public async Task<ActionResult<QuizResultListVm>> Get(Guid id)
+    {
+        var query = new GetQuizResultListQuery
+        {
+            UserId = id
+        };
+
+        var quizResults = await Mediator.Send(query);
+
+        return Ok(quizResults);
+    }
+
+    [HttpPost("create")]
+    public async Task<ActionResult<Guid>> Create([FromBody] CreateQuizResultDto dto)
+    {
+        var command = mapper.Map<CreateQuizResultCommand>(dto);
+
+        var quizResultId = await Mediator.Send(command);
+
+        return Ok(quizResultId);
+    }
+
+    [HttpDelete("delete/{id:guid}")]
+    public async Task<ActionResult> Delete(Guid id)
+    {
+        var command = new DeleteQuizResultCommand
+        {
+            Id = id
+        };
+
+        await Mediator.Send(command);
+
+        return NoContent();
+    }
+}
---
+++ MelodyHub.WebApi/Models/CreateQuizResultDto.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using MelodyHub.Application.Common.Mappings;
+using MelodyHub.Application.CQRS.QiuzResults.Commands.CreateQuizResult;
+using MelodyHub.Application.CQRS.Quizzes.Commands.CreateQuiz;
+
+namespace MelodyHub.WebApi.Models;
+
+public class CreateQuizResultDto : IMapWith<CreateQuizResultCommand>
+{
+    public Guid UserId { get; set; }
+    public Guid QuizId { get; set; }
+    public int Score { get; set; }
+    public int MaxScore { get; set; }
+
+    public void Mapping(Profile profile)
+        => profile.CreateMap<CreateQuizResultDto, CreateQuizResultCommand>();
+}
